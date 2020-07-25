b46060ef5141bf9aa827e24c735654b391b5477f
michaelJava69 <michael.professional@inbox.com>
2020-07-25 22:24:33 +0100

    Update variables.tf

--- terraform/global/s3/variables.tf
+++ terraform/global/s3/variables.tf
@@ -1,11 +1,11 @@
 variable "bucket_name" {
   description = "The name of the S3 bucket. Must be globally unique.  : terraform-s3-bucket-jenkins"
   type        = string
-  default = "terraform-s3-bucket-jenkins"
+  default = "terraform-s3-bucket-jenkins-pipeline"
 }
 
 variable "table_name" {
   description = "The name of the DynamoDB table. Must be unique in this AWS account.  : terraform-s3-bucket-jenkins-locks"
   type        = string
-  default = "terraform-s3-bucket-jenkins-locks"
+  default = "terraform-s3-bucket-jenkins-pipeline-locks"
 }
